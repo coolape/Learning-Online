25d74e191a78f5345ce9a306ad83e1186d15f349
chenbin <chbin12@126.com>
2019-11-19 14:49:36 +0800

    modify login logic

--- www/js/login.js
+++ www/js/login.js
@@ -2,7 +2,14 @@ document.write('<script src="js/public/load.js" type="text/javascript" ></script
 document.write('<script src="js/public/ready.js" type="text/javascript" ></script>');
 document.write('<script src="js/option/NetProtoLearnClient.js" type="text/javascript" ></script>');
 function ready(){
-
+	console.log("ready");
+	NetProtoLearn.init("http://127.0.0.1:8810/LearningOnline/get",
+	function () {
+		console.log("show hot  wheel");
+	},
+	function () {
+		console.log("hide hot  wheel");
+	});
 }
 
 function onsubmit_login(){
@@ -12,7 +19,16 @@ function onsubmit_login(){
 	var lgidval = lgid.val();
 	var lgpdval = lgpd.val();
 	if(lgidval.length > 0 && lgpdval.length >0){
-		NetProtoLearn.send.login(lgidval,lgpdval);
+		NetProtoLearn.send.login(lgidval,lgpdval, function (result, status, xhr) {
+			console.log(result)
+			if (result.retInfor.code != 1) {
+				console.log(result.retInfor.msg);
+			} else {
+				NetProtoLearn.setSession(result.sessionID)
+				console.log(result.custInfor)
+			}
+
+		});
 	}
 	return r;
 }
