6325dff9f3b5530aaf4f470de76fb2ee9b234ac8
canyon <gongyanghui1986@163.com>
2019-11-30 16:36:18 +0800

    避免跨越加载文件

--- www/js/index_01.js
+++ www/js/index_01.js
@@ -1,22 +1,24 @@
 document.write('<script src="js/must.js" type="text/javascript" ></script>');
 
 function ready() {
-    XMLLoad.loadTxt('xml/index_01.xml', function(txt) {
-        $("#main_contect").append(txt);
-        var _ses = NetProtoLearn.getSession();
-        var _isLogin = (null != _ses) && ("" != _ses)
-        var _str = "登录";
-        var _jA = $("nav a[type='user_statue']");
-        _jA.attr("onclick", "javascripte:ihome_01.vw_login();");
-        if (_isLogin) {
-            _str = localStorage.getItem("uname") + ",登出";
-            _jA.attr("onclick", "javascripte:ihome_01.vw_logout();");
-        }
-        _jA.html(_str);
-    });
+    ihome_01._onInit();
 }
 
 var ihome_01 = {};
+
+ihome_01._onInit = function() {
+    var _ses = NetProtoLearn.getSession();
+    var _isLogin = (null != _ses) && ("" != _ses)
+    var _str = "登录";
+    var _jA = $("nav a[type='user_statue']");
+    _jA.attr("onclick", "javascripte:ihome_01.vw_login();");
+    if (_isLogin) {
+        _str = localStorage.getItem("uname") + ",登出";
+        _jA.attr("onclick", "javascripte:ihome_01.vw_logout();");
+    }
+    _jA.html(_str);
+}
+
 ihome_01.vw_login = function() {
     window.location.href = "login.html";
 }
